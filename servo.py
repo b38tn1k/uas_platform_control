5be01166d7375ad19acf8c68174130523864cf33
James Carthew <jcarthew@ford.com>
2017-05-25 18:52:05 +0000

    servo

---
+++ servo.py
@@ -0,0 +1,30 @@
+import Adafruit_BBIO.PWM as PWM
+import time
+
+class Servo():
+    def __init__(self, pin, duty_min=3, duty_max=14.5):
+        self.pin = pin
+        self.duty_min = duty_min
+        self.duty_max = duty_max
+        self.duty_span = duty_max - duty_min
+        self.active = False
+
+    def start(self):
+        if self.active is False:
+            PWM.start(self.pin, (100 - self.duty_min), 60.0)
+            self.active = True
+
+    def stop(self):
+        PWM.stop(self.pin)
+        PWM.cleanup()
+        self.active = False
+
+    def demo(self):
+        for i in range(10, 100, 10):
+            self.start()
+            print ("value: " + str(i))
+            duty = 100 - ((float(i) / 180) * self.duty_span + self.duty_min)
+            PWM.set_duty_cycle(self.pin, duty)
+            time.sleep(1)
+
+
